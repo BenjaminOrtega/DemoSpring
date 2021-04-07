7600b68efc3ea9c41c2c13f509eb3ebea16fab8d
BenjaminOrtega <eduardobenjamin.cortes@outlook.com>
2021-04-07 00:16:48 -0500

    plantillas y validaciones en thymeleaf

--- src/main/java/mx/com/eb/web/ControllerInicio.java
+++ src/main/java/mx/com/eb/web/ControllerInicio.java
@@ -1,8 +1,11 @@
 package mx.com.eb.web;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,7 +35,10 @@ public class ControllerInicio{
 	}
 	
 	@PostMapping("/guarda")
-	public String guardar(Persona persona) {
+	public String guardar(@Valid Persona persona, Errors errors) {
+		if(errors.hasErrors()) {
+			return "modificar";
+		}
 		personaService.guardar(persona);
 		return "redirect:/";
 	}
